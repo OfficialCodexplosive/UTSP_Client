d85a401f7c3a7588cba3c344bc140659e51ca5a2
d.neuroth <d.neuroth@fz-juelich.de>
2022-09-05 19:07:44 +0200

    LPG result file filter for external resolution now creates an error if the resolution is 60 s

--- utspclient/result_file_filters.py
+++ utspclient/result_file_filters.py
@@ -26,11 +26,13 @@ class LPGFilters:
         specified load type, in the external resolution. The resolution specified here must match the
         external resolution specified in the LPG request.
         If the resolution is 60 s, the internal resolution file name is returned"""
-        if resolution_in_s == 60:
-            # special case: external resolution is the same as the internal resolution of the LPG, so
-            # the LPG does not generate extra files --> choose the corresponding file with internal
-            # resolution instead
-            return LPGFilters.sum_hh1(load_type, json=json)
+        assert resolution_in_s != 60, (
+            "The external resolution must not be 60s when using this file name, ",
+            "because that is the internal resolution of the LPG and extra files for external resolution are not created. ",
+            "Use the filename for the internal resolution files instead.",
+        )
+        # special case: external resolution is the same as the internal resolution of the LPG, so
+        # the LPG does not generate extra files
         ext = "json" if json else "csv"
         return "Results/SumProfiles_{resolution_in_s}s.HH1.{load_type}.{ext}".format(
             load_type=load_type, resolution_in_s=resolution_in_s, ext=ext
