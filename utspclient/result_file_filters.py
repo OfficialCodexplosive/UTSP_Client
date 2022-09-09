b8995976e51503950e57e15cb45c91af575a000a
d.neuroth <d.neuroth@fz-juelich.de>
2022-09-09 12:54:26 +0200

    added LPG result file filters for transportation files

--- utspclient/result_file_filters.py
+++ utspclient/result_file_filters.py
@@ -4,6 +4,10 @@ and storing unneeded files
 """
 
 
+from typing import Dict
+from utspclient.datastructures import ResultFileRequirement
+
+
 class LPGFilters:
     """
     Provides result file names for the LPG
@@ -43,6 +47,40 @@ class LPGFilters:
         return "Results/SumProfiles_{resolution_in_s}s.HH1.{load_type}.{ext}".format(
             load_type=load_type, resolution_in_s=resolution_in_s, ext=ext
         )
+
+    class Cars:
+        _template = "Car {i}, {power}kW Charging Power, avg. Speed {speed} km h"
+        CAR1 = _template.format(i=1, power=22, speed=30)
+        CAR2 = _template.format(i=2, power=22, speed=30)
+        CAR3 = _template.format(i=3, power=22, speed=60)
+        CAR4 = _template.format(i=4, power=22, speed=60)
+        ALL = [CAR1, CAR2, CAR3, CAR4]
+
+    @staticmethod
+    def car_state(car: str) -> str:
+        """Result file names for car states"""
+        return f"Results/Carstate.{car}.HH1.json"
+
+    @staticmethod
+    def all_car_states_optional() -> Dict[str, ResultFileRequirement]:
+        """Helper function to get any created car state file."""
+        return {
+            LPGFilters.car_state(c): ResultFileRequirement.OPTIONAL
+            for c in LPGFilters.Cars.ALL
+        }
+
+    @staticmethod
+    def driving_distance(car: str) -> str:
+        """Result file names for driving distances"""
+        return f"Results/DrivingDistance.{car}.HH1.json"
+
+    @staticmethod
+    def all_driving_distances_optional() -> Dict[str, ResultFileRequirement]:
+        """Helper function to get any created driving distance file."""
+        return {
+            LPGFilters.driving_distance(c): ResultFileRequirement.OPTIONAL
+            for c in LPGFilters.Cars.ALL
+        }
 
     class BodilyActivity:
         """Result file names for bodily activity"""
