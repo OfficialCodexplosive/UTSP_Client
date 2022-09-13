a66a3056d63ad976f056cc99bf7fd2f040993254
d.neuroth <d.neuroth@fz-juelich.de>
2022-09-13 17:08:13 +0200

    increased version number to 0.1.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(dir_path, "README.md"), "r") as fh:
 
 setuptools.setup(
     name="utspclient",
-    version="0.1.0",
+    version="0.1.1",
     author="David Neuroth",
     author_email="d.neuroth@fz-juelich.de",
     description="Universal Time Series Provider Client",
